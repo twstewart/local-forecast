e7cc264f288460a4c7bd26fffdd9ada031f43ed5
tdubsxc <trdubs90@gmail.com>
2023-01-24 17:46:17 -0500

    Finished working on the weather parsing functions.

--- app.js
+++ app.js
@@ -4,3 +4,6 @@ const latLng = [10, 10];
 const tz = new Intl.DateTimeFormat().resolvedOptions().timeZone;
 
 const { current, daily, hourly } = await fetchWeatherData(latLng[0], latLng[1], tz);
+console.log("🚀 | file: app.js:7 | current", current);
+console.log("🚀 | file: app.js:7 | daily", daily);
+console.log("🚀 | file: app.js:7 | hourly", hourly);
--- js/forecast.js
+++ js/forecast.js
@@ -11,8 +11,46 @@ export default async function fetchWeatherData(lat, lng, tz) {
   };
 }
 
-function parseCurrentWeather() {}
+function parseCurrentWeather({ current_weather, daily }) {
+  const { temperature: currentTemp, windspeed: windSpeed, weathercode: iconCode } = current_weather;
+  const {
+    temperature_2m_max,
+    temperature_2m_min,
+    apparent_temperature_max,
+    apparent_temperature_min,
+    precipitation_sum,
+  } = daily;
 
-function parseDailyWeather() {}
+  return {
+    currentTemp: Math.round(currentTemp),
+    lowTemp: Math.round(temperature_2m_min[0]),
+    lowFeelsLike: Math.round(apparent_temperature_min[0]),
+    highTemp: Math.round(temperature_2m_max[0]),
+    highFeelsLike: Math.round(apparent_temperature_max[0]),
+    windSpeed: Math.round(windSpeed),
+    precipitation: Math.round(precipitation_sum[0] * 100) / 100,
+    iconCode,
+  };
+}
 
-function parseHourlyWeather() {}
+function parseDailyWeather({ daily }) {
+  return daily.time.map((t, i) => ({
+    timestamp: t * 1000,
+    iconCode: daily.weathercode[i],
+    maxTemp: daily.temperature_2m_max[i],
+    minTemp: daily.temperature_2m_min[i],
+  }));
+}
+
+function parseHourlyWeather({ hourly, current_weather }) {
+  return hourly.time
+    .map((t, i) => ({
+      timestamp: t * 1000,
+      iconCode: hourly.weathercode[i],
+      temp: Math.round(hourly.temperature_2m[i]),
+      feelsLike: Math.round(hourly.apparent_temperature[i]),
+      windSpeed: Math.round(hourly.windspeed_10m[i]),
+      precipitation: Math.round(hourly.precipitation[i] * 100) / 100,
+    }))
+    .filter(({ timestamp }) => timestamp >= current_weather.time * 1000);
+}
